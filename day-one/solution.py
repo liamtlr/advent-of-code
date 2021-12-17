2839e175fe7d82aff7ea4cab0ae6cc91b088a0de
Liam Taylor <liam@piclo.energy>
2021-12-17 18:37:36 +0000

    completes day one challenge two

--- day-one/solution.py
+++ day-one/solution.py
@@ -9,4 +9,20 @@ def get_increased_reading_count(readings: list) -> int:
         if int(reading) < int(readings[next_index])
     )
     return increased_readings
+
+
 print(get_increased_reading_count(readings))
+
+
+def get_sliding_increased_reading_count(readings: list) -> int:
+    """Return a count of the number of increased readings based on the sliding total."""
+    increased_readings: int = sum(
+        1
+        for next_index, reading in enumerate(readings[:-4])
+        # Compare the current reading (the one to leave the grouping) with the
+        # reading 3 to the left (the one to join the grouping).
+        if int(reading) < int(readings[next_index + 4])
+    )
+    return increased_readings
+
+print(get_sliding_increased_reading_count(readings))
