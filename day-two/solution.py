a8037d9f5ea8090855d776e27e0a14c6058529c1
Liam Taylor <liam@piclo.energy>
2021-12-17 19:56:19 +0000

    day-two: completes challenge two

--- day-two/solution.py
+++ day-two/solution.py
@@ -8,12 +8,14 @@ with open('input_data.txt', 'r') as infile:
 def navigate(
         movements: list,
         start_depth: int=0,
-        start_horizontal: int=0
+        start_horizontal: int=0,
+        start_aim: int=0,
 ) -> Tuple[int, int]:
     """Apply movements to the depth and horizontal values."""
     NET_VALUES = {
         'depth': start_depth,
-        'horizontal': start_horizontal
+        'horizontal': start_horizontal,
+        'aim': start_aim,
     }
     DIRECTION_ADJUSTMENT_MAP: dict = {
         'forward': 1,
@@ -22,17 +24,22 @@ def navigate(
     }
     DIRECTION_MAP: dict = {
         'forward': 'horizontal',
-        'up': 'depth',
-        'down': 'depth',
+        'up': 'aim',
+        'down': 'aim',
     }
 
     for movement in movements:
         direction, value_str = movement.split(' ')
+        value = int(value_str)
         direction_dimension: str = DIRECTION_MAP[direction]
         target_value: int = NET_VALUES[direction_dimension]
-        adjusted_value: int = int(value_str) * DIRECTION_ADJUSTMENT_MAP[direction]
+        adjusted_value: int = value * DIRECTION_ADJUSTMENT_MAP[direction]
         target_value += adjusted_value
         NET_VALUES[direction_dimension] = target_value
+
+        if direction == 'forward':
+            depth_increase = NET_VALUES['aim'] * value
+            NET_VALUES['depth'] += depth_increase
 
     return NET_VALUES['depth'], NET_VALUES['horizontal']
 
