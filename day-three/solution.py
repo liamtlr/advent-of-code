5995fd6dd1ca823336a717d46a609e4cfef7d2f6
Liam Taylor <liam@piclo.energy>
2021-12-18 11:52:03 +0000

    day-three: completes challenge two

--- day-three/solution.py
+++ day-three/solution.py
@@ -1,5 +1,6 @@
 from functools import cached_property
-from statistics import mode
+from statistics import multimode, mode
+from typing import Callable
 
 
 class DiagnosticReport:
@@ -14,8 +15,8 @@ class DiagnosticReport:
     @cached_property
     def gamma_rate(self) -> str:
         """Calculate the gamma rate."""
-        self._transposed_data = self._transpose_data(self.readings)
-        most_commons: list = [mode(column) for column in self._transposed_data]
+        transposed_data: list = self._transpose_data(self.readings)
+        most_commons: list = [mode(column) for column in transposed_data]
         gamma_rate: str = "".join(most_commons)
         return gamma_rate
 
@@ -32,10 +33,32 @@ class DiagnosticReport:
     @cached_property
     def power_consumption(self) -> int:
         """Calculate the power consumption."""
-        gamma_rate_int: int  = self._binary_string_to_int(self.gamma_rate)
-        epsilon_rate_int: int  = self._binary_string_to_int(self.epsilon_rate)
+        gamma_rate_int: int = self._binary_string_to_int(self.gamma_rate)
+        epsilon_rate_int: int = self._binary_string_to_int(self.epsilon_rate)
         power_consumption: int =  gamma_rate_int * epsilon_rate_int
         return power_consumption
+
+    @cached_property
+    def o2_generator_rating(self) -> str:
+        """Calculate the oxygen generator rating."""
+        remaining: list = list(self.readings)
+        rating: str = self._extract_rating(remaining, 0, self._o2_reducer)
+        return rating
+
+    @cached_property
+    def co2_scrubber_rating(self) -> str:
+        """Calculate the carbon dioxide scrubber rating."""
+        remaining: list = list(self.readings)
+        rating: str = self._extract_rating(remaining, 0, self._co2_reducer)
+        return rating
+
+    @cached_property
+    def life_support_rating(self) -> int:
+        """Calculate the life_support rating."""
+        o2_gen_int: int = self._binary_string_to_int(self.o2_generator_rating)
+        co2_scrub_int: int = self._binary_string_to_int(self.co2_scrubber_rating)
+        life_support: int =  o2_gen_int * co2_scrub_int
+        return life_support
 
     def _transpose_data(self, readings: list) -> list:
         """Transpose a 2D list."""
@@ -47,5 +70,44 @@ class DiagnosticReport:
         binary_as_int: int = int(binary_string, 2)
         return binary_as_int
 
+    def _get_column(self, column: int, table: list) -> list:
+        """Extract a column from a 2D list."""
+        return [row[column] for row in table]
 
-print(DiagnosticReport('input_data.txt').power_consumption)
+    def _extract_rating(
+            self,
+            readings: list,
+            index: int,
+            reducer: Callable
+    ) -> str:
+        """Extract a single rating from a set using a given reducer."""
+        column: list = self._get_column(index, readings)
+        target: str = reducer(column)
+        selected: list = [
+            reading for reading in readings
+            if reading[index] == target
+        ]
+        if len(selected) == 1:
+            return selected[0]
+        else:
+            return self._extract_rating(selected, index + 1, reducer)
+
+    def _o2_reducer(self, column: list) -> str:
+        """"Get the target value for discerning o2 data from readings."""
+        most_common_list: list = multimode(column)
+        most_common: str = max(most_common_list)
+        return most_common
+
+    def _co2_reducer(self, column: list) -> str:
+        """Get the target value for discerning co2 data from readings."""
+        most_common_list: list = multimode(column)
+        if len(most_common_list) == 2:
+            target_val: str = '0'
+        else:
+            target_val: str = '0' if most_common_list[0] == '1' else '1'
+        return target_val
+
+
+report = DiagnosticReport('input_data.txt')
+print(report.power_consumption)
+print(report.life_support_rating)
