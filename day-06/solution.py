2fa492af89acca7d9d0ee39d5fe3d256ad94af19
Liam Taylor <liam@piclo.energy>
2021-12-20 14:42:11 +0000

    day-06: completes challenge 2

--- day-06/solution.py
+++ day-06/solution.py
@@ -1,62 +1,69 @@
-from typing import List
-
-
-class LanternFish:
-
-    """Class encapsulation logic for a lanternfish"""
-
-    REGULAR_GESTATION_PERIOD = 6
-
-    def __init__(self, initial_days_till_birth: int):
-        """Set the initial day till birth."""
-        self.days_till_birth = initial_days_till_birth
-
-    @property
-    def ready_to_give_birth(self) -> bool:
-        """Determiens whether the fish is ready to give birth"""
-        return self.days_till_birth == 0
-
-    def handle_day(self):
-        """Handle the day in the life of a wee lanternfish."""
-        if self.ready_to_give_birth:
-            self.days_till_birth = self.REGULAR_GESTATION_PERIOD
-        else:
-            self.days_till_birth -= 1
+from typing import Dict, List
 
 
 class SeaBed:
 
     """Logic encapsulating a bed of the sea."""
 
-    NEWBORN_GESTATION_PERIOD = 8
+    NEWBORN_GESTATION_PERIOD = 9
+    REGULAR_GESTATION_PERIOD = 7
 
     def __init__(self, infile_path: str):
         """Read the vent data."""
-        self.bed = {}
+        self.day: int = 0
+        self.total_fishes: int = 0
+        self.birthing_plan: Dict[int, int] = {}
         with open(infile_path, 'r') as infile:
             raw_data: list = infile.readlines()
             data: list = raw_data[0].split(',')
-            self.fishies: list[LanternFish] = [
-                LanternFish(int(datum))
-                for datum in data
-            ]
+            for fish in data:
+                self._handle_newborns(1, int(fish))
 
     def fishes_after_n_days(self, days: int) -> int:
         """Get the number of fishes after the given number of days."""
-        for _ in range(days):
-            self.day_passes()
-        return len(self.fishies)
+        for day in range(days):
+            self.day_passes(day)
+            try:
+                del self.birthing_plan[day]
+            except KeyError:
+                pass
+        return self.total_fishes
 
-    def day_passes(self) -> None:
-        """Handle the passing of a day on the fish population"""
-        newborn_fishies: List[LanternFish] = []
-        for fish in self.fishies:
-            if fish.ready_to_give_birth:
-                newborn: LanternFish = LanternFish(self.NEWBORN_GESTATION_PERIOD)
-                newborn_fishies.append(newborn)
-            fish.handle_day()
-        self.fishies.extend(newborn_fishies)
+    def day_passes(self, day: int) -> None:
+        """Handle the passing of a day on the fish population."""
+        try:
+            fish_to_give_birth: int = self.birthing_plan[day]
+            self._handle_newborns(fish_to_give_birth)
+            self._handle_parents(fish_to_give_birth)
+        except KeyError:
+            self.day += 1
+            return
+        self.day += 1
 
+    def _handle_newborns(
+            self,
+            newborn_count: int,
+            days_till_birth: int = NEWBORN_GESTATION_PERIOD
+    ) -> None:
+        """Insert the newborn into the birthing plan."""
+        due_date: int = self.day + days_till_birth
+        self._insert_into_birthing_plan(newborn_count, due_date)
+        self.total_fishes += newborn_count
+
+    def _handle_parents(
+            self,
+            parent_count: int,
+    ) -> None:
+        """Re-insert the parents into the birthing plan."""
+        due_date: int = self.day + self.REGULAR_GESTATION_PERIOD
+        self._insert_into_birthing_plan(parent_count, due_date)
+
+    def _insert_into_birthing_plan(self, count: int, due_date: int) -> None:
+        """Insert the given number of pregnant fishes as per due date."""
+        if due_date not in self.birthing_plan:
+            self.birthing_plan[due_date] = count
+        else:
+            self.birthing_plan[due_date] += count
 
 
 print(SeaBed('input_data.txt').fishes_after_n_days(256))
