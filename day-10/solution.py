ee060b98c1d2a47b4452ce1a4cfe95863398c8b5
Liam Taylor <liam@piclo.energy>
2021-12-23 11:45:39 +0000

    day-09: completes challenge 2

--- day-10/solution.py
+++ day-10/solution.py
@@ -1,26 +1,33 @@
+from functools import reduce
 from typing import Dict, List, Optional
 
 class BraceSyntaxError(Exception):
     """Custom exception for when a brace syntax error incurred"""
     pass
 
+
 class SyntaxChecker:
 
     """Class for encapsulating brace syntax logic."""
 
     OPENERS = {'(', '{', '[', '<'}
-
     CLOSER_MAP: Dict[str, str] = {
         '{': '}',
         '[': ']',
         '(': ')',
         '<': '>',
     }
-    POINTS_MAP: Dict[str, str] = {
+    ERROR_POINTS_MAP: Dict[str, str] = {
         ')': 3,
         ']': 57,
         '}': 1197,
         '>': 25137,
+    }
+    COMPLETION_POINTS_MAP: Dict[str, str] = {
+        '(': 1,
+        '[': 2,
+        '{': 3,
+        '<': 4,
     }
 
     def __init__(self, path: str) -> List[str]:
@@ -30,10 +37,34 @@ class SyntaxChecker:
         self.to_be_closed: List[str] = []
         self.to_be_closed_next: Optional[str] = None
         self.points: int = 0
+        self.to_complete: List[str] = []
+        self._parse_rows()
 
-    def calculate_syntax_errors(self) -> int:
+    @property
+    def syntax_errors(self) -> int:
         """Calculate syntax error scores."""
+        return self.points
+
+    @property
+    def completion_score(self) -> int:
+        """Calculate the completion score."""
+        scores: List[int] = [
+            reduce(
+                lambda acc, curr: (acc * 5) + self.COMPLETION_POINTS_MAP[curr],
+                unclosed,
+                0
+            )
+            for unclosed in self.to_complete
+        ]
+        sorted_scores = sorted(scores)
+        middle_score = len(scores) // 2
+        return sorted_scores[middle_score]
+
+    def _parse_rows(self):
+        """Parse the data"""
         for row in self.data:
+            self.to_be_closed_next = None
+            self.to_be_closed = []
             for char in row:
                 if char in self.OPENERS:
                     self._handle_opening(char)
@@ -42,21 +73,33 @@ class SyntaxChecker:
                         self._check_closure(char)
                     except BraceSyntaxError:
                         break
-        return self.points
+            else:
+                self._handle_incompletion()
 
     def _handle_opening(self, opener: str) -> None:
         """Handle a newly opened set of braces."""
-        self.to_be_closed.append(self.to_be_closed_next)
+        if self.to_be_closed_next is not None:
+            self.to_be_closed.append(self.to_be_closed_next)
         self.to_be_closed_next = opener
 
     def _check_closure(self, char: str) -> None:
         """Check braces have been close correctly."""
         closer: str = self.CLOSER_MAP[self.to_be_closed_next]
         if char != closer:
-            self.points += self.POINTS_MAP[char]
+            self.points += self.ERROR_POINTS_MAP[char]
             raise BraceSyntaxError
         else:
-            self.to_be_closed_next = self.to_be_closed.pop()
+            if self.to_be_closed:
+                self.to_be_closed_next = self.to_be_closed.pop()
+            else:
+                self.to_be_closed_next = None
+
+    def _handle_incompletion(self) -> None:
+        """Handle the unclosed braces."""
+        self.to_be_closed.append(self.to_be_closed_next)
+        reversed_for_closure: List[str] = reversed(self.to_be_closed)
+        self.to_complete.append(reversed_for_closure)
 
 
-print(SyntaxChecker('input_data.txt').calculate_syntax_errors())
+print(SyntaxChecker('input_data.txt').syntax_errors)
+print(SyntaxChecker('input_data.txt').completion_score)
