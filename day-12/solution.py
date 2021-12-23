0201f2f740ea497da391f0b312bd59b15de00c22
Liam Taylor <liam@piclo.energy>
2021-12-23 17:34:38 +0000

    day-12: completes challenge 2

--- day-12/solution.py
+++ day-12/solution.py
@@ -1,5 +1,4 @@
-from functools import reduce
-from typing import Dict, List, Optional, Tuple
+from typing import Dict, List
 
 
 class CaveTraverser:
@@ -24,9 +23,10 @@ class CaveTraverser:
                     self.small_caves.add(do)
                 self._add_caves_to_network(ot, do)
 
-    def find_paths(self) -> List[Tuple]:
+    def find_paths(self) -> int:
         """Find paths from start to end."""
-        self._build_path(self.START_CAVE, [])
+        preceding_data: dict = {'double_small_cave': False, 'preceders': set([])}
+        self._build_path(self.START_CAVE, preceding_data)
         return len(self.paths)
 
     def _is_small_cave(self, cave: str) -> bool:
@@ -44,20 +44,38 @@ class CaveTraverser:
         else:
             self.cave_network[to_cave].add(from_cave)
 
-    def _build_path(self, cave: str, preceders: List[str]) -> Optional[List[str]]:
+    def _build_path(self, cave: str, preceders: dict) -> None:
         """Get the path to the end from the given cave."""
         connected_caves = self.cave_network[cave]
         for connected_cave in connected_caves:
-            fresh_preceders: List[str] = list(preceders)
-            if connected_cave in self.small_caves and connected_cave in preceders:
+            fresh_preceders: set = set(preceders['preceders'])
+            fresh_bool: bool = preceders['double_small_cave']
+            if connected_cave == self.START_CAVE:
                 continue
+            if (
+                connected_cave in self.small_caves
+                and connected_cave in fresh_preceders
+                and fresh_bool
+            ):
+                continue
+            elif (
+                connected_cave in self.small_caves
+                and connected_cave in fresh_preceders
+                and not fresh_bool
+            ):
+                fresh_bool = True
             if connected_cave == self.END_CAVE:
-                fresh_preceders.extend([cave, connected_cave])
+                fresh_preceders.add(cave)
+                fresh_preceders.add(connected_cave)
                 self.paths.append(fresh_preceders)
                 continue
             else:
-                fresh_preceders.append(cave)
-                self._build_path(connected_cave, fresh_preceders)
+                fresh_preceders.add(cave)
+                preceder_dict: dict = {
+                    'preceders': fresh_preceders,
+                    'double_small_cave': fresh_bool
+                }
+                self._build_path(connected_cave, preceder_dict)
 
 
 print(CaveTraverser('input_data.txt').find_paths())
